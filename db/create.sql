87059ae762533eea9e7450c12fd047ee64dc7065
hoenirvili <hoenirvili@gmail.com>
2017-08-23 00:04:11 +0300

    More sql
    Signed-off-by: hoenirvili <hoenirvili@gmail.com>

--- db/create.sql
+++ db/create.sql
@@ -9,24 +9,42 @@ FLUSH PRIVILEGES;
 # select database
 use Vor;
 
-CREATE TABLE OR REPLACE User(
-    Id          INT UNSIGNED    NOT NULL AUTO_INCREMENT,
-    Username    VARCHAR(50)     NOT NULL,
-    Password    VARCHAR(512)    NOT NULL,
-
-    PRIMARY_KEY(Id),
+CREATE OR REPLACE TABLE User(
+    Id              INT UNSIGNED    NOT NULL AUTO_INCREMENT PRIMARY_KEY
+    Username        VARCHAR(50)     NOT NULL,
+    Password        VARCHAR(512)    NOT NULL,
+    Cookie          VARCHAR(512)    NOT NULL,
+    CookieCreated   DATETIME        NOT NULL,
+    CookieExpires   DATETIME        NOT NULL,
 );
 
-CREATE UNIQUE INDEX User_Index
-    ON User (Id);
+CREATE OR REPLACE TABLE Article (
+    Id          INT UNSIGNED    NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+    Title       VARCHAR(350)    NOT NULL,
+    Time        DATE            NOT NULL,
+    Content     LONGTEXT        NOT NULL,
+    Tags        VARCHAR(200),
+    AuthorId    INT UNSIGNED    NOT NULL,
 
-
-CREATE TABLE OR REPLACE Image(
-    Id, INT UNSIGNED            NOT NULL AUTO_INCREMENT,
-    Localtion VARCHAR(256)      NOT NULL,
-
-    PRIMARY_KEY(Id),
+    FOREIGN KEY (AuthorId)  REFERENCES User(Id),
 );
 
-CREATE UNIQUE INDEX Image_Index
-    ON Image (Id);
+CREATE OR REPLACE TABLE ArticleComment(
+    ArticleId INT UNSIGNED NOT NULL,
+    CommentId INT UNSIGNED NOT NULL,
+
+    FOREIGN KEY (ArticleId)  REFERENCES Article(Id),
+    FOREIGN KEY (CommentId)  REFERENCES Comment(Id),
+);
+
+CREATE OR REPLACE TABLE Comment (
+    Id      INT UNSIGNED    NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+    Name    VARCHAR(50)     DEFAULT 'Anonymous',
+    Time    DATE            NOT NULL,
+    Email   VARCHAR(50),
+    Comment VARCHAR(512)    NOT NULL
+);
+
+CREATE UNIQUE INDEX User_Index      ON User (Id);
+CREATE UNIQUE INDEX Comment_Index   ON Comment(Id);
+CREATE UNIQUE INDEX Image_Index     On Image (Id);
--- db/delete.sql
+++ db/delete.sql
@@ -1,13 +1,11 @@
 # drop index from table
-ALTER TABLE User
-DROP INDEX IF EXISTS User_Index;
-ALTER TABLE Image
-DROP INDEX IF EXISTS Image_Index;
-
+ALTER TABLE User DROP INDEX IF EXISTS User_Index;
+ALTER TABLE Comment DROP INDEX IF EXISTS Comment_Index;
+ALTER TABLE Article DROP INDEX IF EXISTS Article_Index;
 # drop table;
 DROP TABLE IF EXISTS User;
-DROP TABLE IF EXISTS Image;
-
+DROP TABLE IF EXISTS Comment;
+DROP TABLE IF EXISTS Article;
 
 # delete the database
 DROP DATABASE IF EXISTS Vor;
